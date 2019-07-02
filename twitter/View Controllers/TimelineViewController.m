7e81dffe1d7c99b38f92d7dd8f6915e9a8efcbfd
Kyle Fullen <kfullen@fb.com>
2019-07-01 18:03:07 -0700

    working on it :(

--- twitter/View Controllers/TimelineViewController.m
+++ twitter/View Controllers/TimelineViewController.m
@@ -9,34 +9,67 @@
 #import "TimelineViewController.h"
 #import "APIManager.h"
 
-@interface TimelineViewController ()
-
+@interface TimelineViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (strong,nonatomic) NSArray *tweets;
+@property (weak, nonatomic) IBOutlet UITableView *tweetTableView;
 @end
 
 @implementation TimelineViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.tweetTableView.dataSource = self;
+    self.tweetTableView.delegate = self;
     
+    [self fetchTweets];
+    
+}
+
+- (void) fetchTweets {
     // Get timeline
     [[APIManager shared] getHomeTimelineWithCompletion:^(NSArray *tweets, NSError *error) {
         if (tweets) {
             NSLog(@"😎😎😎 Successfully loaded home timeline");
-            for (NSDictionary *dictionary in tweets) {
-                NSString *text = dictionary[@"text"];
+            for (NSDictionary *tweet in tweets) {
+                NSString *text = tweet[@"text"];
                 NSLog(@"%@", text);
             }
         } else {
             NSLog(@"😫😫😫 Error getting home timeline: %@", error.localizedDescription);
         }
     }];
-}
+    
 
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:
+(NSInteger)section {
+    return self.tweets.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:
+(NSIndexPath *)indexPath {
+    TweetCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TweetCell"];
+    
+    NSDictionary *tweet = self.tweets[indexPath.row];
+    
+    cell.tweetLabel.text = tweet[@"text"];
+    cell.userLabel.text = movie[@"user"];
+    NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
+    NSString *posterURLString = movie[@"poster_path"];
+    NSString *fullURLString = [baseURLString stringByAppendingString:posterURLString];
+    
+    NSURL *posterURL = [NSURL URLWithString:fullURLString];
+    cell.posterView.image = nil;
+    [cell.posterView setImageWithURL:posterURL];
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
+    
+    return cell;
+}
 /*
 #pragma mark - Navigation
 
---
+++ twitter/Views/TweetCell.h
@@ -0,0 +1,25 @@
+//
+//  TweetCell.h
+//  twitter
+//
+//  Created by kfullen on 7/1/19.
+//  Copyright © 2019 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TweetCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
+@property (weak, nonatomic) IBOutlet UILabel *userLabel;
+@property (weak, nonatomic) IBOutlet UILabel *handleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *createdAtLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tweetLabel;
+@property (weak, nonatomic) IBOutlet UIButton *repliesButton;
+@property (weak, nonatomic) IBOutlet UIButton *retweetsButton;
+@property (weak, nonatomic) IBOutlet UIButton *likesButton;
+@property (strong,nonatomic) NSString *tweet;
+@end
+
+NS_ASSUME_NONNULL_END
